1ca04dcb44a8b30b98cef820e596596dca71b53c
shawnsky <5835007>
2020-02-26 14:19:47 +0800

    test board apis

---
+++ tequila/src/api/index.js
@@ -0,0 +1,8 @@
+const base = "/api";
+const api = {
+  Login: base + "/auth/login",
+  Register: base + "/auth/register",
+  Board: base + "/board"
+};
+
+export default api;
--- tequila/src/components/admin/board/AdminBoardView.vue
+++ tequila/src/components/admin/board/AdminBoardView.vue
@@ -1,19 +1,50 @@
 <template>
   <div>
     <h3>在此编辑系统公告</h3>
-    <a-textarea placeholder="系统公告..." :rows="6" v-model="data" />
-    <a-button type="primary" style="margin: 32px 0 0 0; float: right"
+    <a-textarea placeholder="系统公告..." :rows="6" v-model="content" />
+    <a-button
+      type="primary"
+      style="margin: 32px 0 0 0; float: right"
+      @click="commitNewContent"
+      :loading="loading"
       >提交</a-button
     >
   </div>
 </template>
 
 <script>
+import api from "@/api/index";
+import axios from "axios";
 export default {
   data() {
     return {
-      data: ""
+      content: "",
+      loading: false
     };
+  },
+  mounted() {
+    axios
+      .get(api.Board)
+      .then(response => {
+        this.content = response.data.data.content;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  methods: {
+    commitNewContent() {
+      this.loading = true;
+      axios
+        .put(api.Board, {
+          content: this.content
+        })
+        .then(response => {
+          if (response.data.code == 1) {
+            this.loading = false;
+          }
+        });
+    }
   }
 };
 </script>
