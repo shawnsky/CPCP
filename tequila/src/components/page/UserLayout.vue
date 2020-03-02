d252521812676e669c5b5ea0b2391517349b2ad9
shawnsky <5835007@qq.com>
2020-03-02 21:45:08 +0800

    add logout

--- tequila/src/components/page/UserLayout.vue
+++ tequila/src/components/page/UserLayout.vue
@@ -10,7 +10,9 @@
         <a-menu-item><Logo title="高校心理咨询平台" /> </a-menu-item>
 
         <a-menu-item key="board">
-          <router-link to="/board"><a-icon type="notification" /> 公告 </router-link>
+          <router-link to="/board"
+            ><a-icon type="notification" /> 公告
+          </router-link>
         </a-menu-item>
 
         <a-menu-item key="posts">
@@ -18,27 +20,36 @@
         </a-menu-item>
 
         <a-menu-item key="test">
-          <router-link to="/test/intro"> <a-icon type="project" /> 在线测评</router-link>
+          <router-link to="/test/intro">
+            <a-icon type="project" /> 在线测评</router-link
+          >
         </a-menu-item>
 
         <a-menu-item key="talk">
-          <router-link to="/talk"><a-icon type="file-text" /> 留言板</router-link>
+          <router-link to="/talk"
+            ><a-icon type="file-text" /> 留言板</router-link
+          >
         </a-menu-item>
 
         <a-menu-item key="profile">
           <router-link to="/user"> <a-icon type="user" />个人中心</router-link>
         </a-menu-item>
+       
+        <a-menu-item style="float: right" @click="logout">
+          <a-icon type="logout" />退出
+        </a-menu-item>
       </a-menu>
     </a-layout-header>
     <a-layout-content style="margin: 48px 0 0 0">
       <a-row>
-        <a-col :md="{span: 16,offset: 4}" :xs="{span: 20, offset: 2}">
-      <div :style="{ background: '#fff', padding: '24px', minHeight: '600px' }">
-        <router-view></router-view>
-      </div>
+        <a-col :md="{ span: 16, offset: 4 }" :xs="{ span: 20, offset: 2 }">
+          <div
+            :style="{ background: '#fff', padding: '24px', minHeight: '600px' }"
+          >
+            <router-view></router-view>
+          </div>
         </a-col>
       </a-row>
-      
     </a-layout-content>
     <a-layout-footer style="text-align: center">
       CPCP 高校心理资讯平台 ©2020
@@ -50,10 +61,17 @@ import Logo from "../Logo";
 export default {
   components: { Logo },
   data() {
-      return {
-        current: [''],
-      };
-    },
+    return {
+      current: [""],
+      username: localStorage.username
+    };
+  },
+  methods: {
+    logout() {
+      localStorage.clear()
+      this.$router.push({path: '/login'})
+    }
+  }
 };
 </script>
 <style>
@@ -65,9 +83,9 @@ export default {
 }
 
 .main {
-    font-size: 16px;
-    margin: 0 8px;
-    line-height: 1.75em;
+  font-size: 16px;
+  margin: 0 8px;
+  line-height: 1.75em;
 }
 
 .appendix {
